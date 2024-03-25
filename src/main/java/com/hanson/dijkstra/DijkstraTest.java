4ea1049ccf980c08ac36dba9aef793c5939a5f00
hanson <huang970622@126.com>
2024-03-25 19:23:42 +0800

    add undirected weight

--- src/main/java/com/hanson/dijkstra/DijkstraTest.java
+++ src/main/java/com/hanson/dijkstra/DijkstraTest.java
@@ -7,19 +7,30 @@ package com.hanson.dijkstra;
 public class DijkstraTest {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        int[][] weight = {
+        // 有向图
+        int[][] directed_weight = {
                 {0, 10, 12, -1, -1, -1},
                 {-1, 0, -1, 16, 25, -1},
                 {4, 3, 0, 12, -1, 8},
                 {-1, -1, -1, 0, 7, -1},
                 {-1, -1, -1, -1, 0, -1},
                 {-1, -1, -1, 2, -1, 0}};
-        String[] str = {"V1", "V2", "V3", "V4", "V5", "V6"};
+
+        // 无向图
+        int[][] undirected_weight = {
+                {0,3,-1,7,-1},
+                {3,0,4,2,-1},
+                {-1,4,0,5,4},
+                {7,2,5,0,6},
+                {-1,-1,4,6,0}};
+
+//        String[] str = {"V1", "V2", "V3", "V4", "V5", "V6"};
+        String[] str = {"V1", "V2", "V3", "V4", "V5"};
         int len = str.length;
         Dijkstra dijkstra = new Dijkstra(len);
         //依次让各点当源点，并调用dijkstra函数
         for (int i = 0; i < str.length; i++) {
-            dijkstra.dijkstra(weight, str, i);
+            dijkstra.dijkstra(undirected_weight, str, i);
         }
     }
 }
